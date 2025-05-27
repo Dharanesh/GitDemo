faa6e7dd965b0461a28dee99ad36e8b25f2d0d3c
User <dharanesh009@gmail.com>
2025-05-27 11:05:38 +0530

    final comit by Asian guys

---
+++ src/test/java/features/masterdemo.java
@@ -0,0 +1,18 @@
+package features;
+
+public class masterdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Git status1");
+		System.out.println("Git status2");
+		System.out.println("Git status3");
+		System.out.println("Git status4");
+		System.out.println("Git status5");
+		System.out.println("Git status6");
+		
+
+	}
+
+}
